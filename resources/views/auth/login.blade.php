04f15bf11993635abb49f2dec772bc57a6f95bed
prasun <prasun.s4n@gmail.com>
2021-09-02 21:13:16 +0545

    added icons and changed text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="/css/login.css" />
     <title>Login</title>
   </head>
@@ -29,18 +30,18 @@
               <input type="email" placeholder="Email" value="{{ old("email") }}" name="email" required>
               <input type="password" placeholder="Password" value="{{ old("password") }}" name="password" required><br>
               <button type="submit">Log in</button>
-              </form> 
+              </form>
               <div class="clearfix"></div>
          </div>
       </div>
       <div class="register">
           <div class="container">
               <i class="fas fa-user-plus fa-5x"></i>
-              <h2>Hello,friend!</h2>
-              <p>Enter your personal details and start journey with us</p>
+              <h2>Hello,there!</h2>
+              <p>Login to continue your further process to consult with the DOC</p>
               <a href="/register"><button>Register <i class="fas fa-arrow-circle-right"></i></button></a>
           </div>
-      </div>  
+      </div>
     </div>
   </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="/css/login.css" />
     <title>Register</title>
   </head>
@@ -31,18 +32,18 @@
               <input type="password" placeholder="Password" value="{{ old("password") }}" name="password"><br>
               <input type="password" placeholder="Confirm Password" name="password_confirmation"><br>
               <button type="submit">Register</button>
-              </form> 
+              </form>
               <div class="clearfix"></div>
          </div>
       </div>
       <div class="register">
           <div class="container">
               <i class="fas fa-user-plus fa-5x"></i>
-              <h2>Hello,friend!</h2>
-              <p>Enter your personal details and start journey with us</p>
+              <h2>Hello,there!</h2>
+              <p>Register your account to consult with your desired Doctor.</p>
               <a href="/login"><button>Login <i class="fas fa-arrow-circle-right"></i></button></a>
           </div>
-      </div>  
+      </div>
     </div>
   </body>
 </html>
