fa55aa8ff22d9985294910a0641ff735556bdec4
prasun <prasun.s4n@gmail.com>
2021-09-04 13:52:47 +0545

    Home page added

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::any('', function () {
     return redirect('/home');
 });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
