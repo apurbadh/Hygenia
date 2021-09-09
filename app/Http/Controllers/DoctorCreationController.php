534e51fb1b1de5cb9b0a57703a7928197d151712
apurbadh <erikduke182@gmail.com>
2021-09-09 13:16:28 +0545

    doctor migrations and model created

---
+++ app/Http/Controllers/DoctorCreationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DoctorCreationController extends Controller
+{
+    public function index(Request $req){
+
+    }
+
+    public function store(Request $req){
+        
+    }
+
+}
---
+++ app/Models/Doctor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return User::find($this->user_id);
+    }
+}
